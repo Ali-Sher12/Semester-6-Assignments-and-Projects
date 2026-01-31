dbbda4201e3746b4d7299bb555f9330ad5349b82
M.Ali Sher <sans42699@gmail.com>
2026-01-31 21:23:12 +0500

    Task1 profiling fully done.

--- PDC/Assignment 1/Task 1/V1.cpp
+++ PDC/Assignment 1/Task 1/V1.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+int dat_percentage[3] = {10,50,100};
 int PRED_POS = 0,TP = 0,FP = 0,TN = 0,FN = 0;
 float e = 2.71828;
 float bias = -0.35;
@@ -27,16 +28,6 @@ struct data_
     }
 };
 
-
-void print_dataset(vector<data_>& point_list)
-{
-    for(int i=0;i<point_list.size();i++)
-    {
-        for(int j=0;j<18;j++)
-            cout<<point_list[i].values[j]<<"   ";
-        cout<<"\n";
-    }
-}
 
 void calculate_linear_score_and_prob_and_set_data(data_& dat)
 {// calculates everything and sets the global values
@@ -63,55 +54,79 @@ void calculate_linear_score_and_prob_and_set_data(data_& dat)
         FN++;
 }
 
-void getOutput(int N)
+void getOutput(int N,double time,int dat)
 {
+    cout<<"\nThreads Used: 1";    
+    cout<<"\nPercentage of data: "<<dat_percentage[dat]<<"%";
+    cout<<"\nChunkSize: Complete";    
     cout<<"\nTotal Records: " << N << "\n";
     cout<<"PRED_POS : " << PRED_POS << "\n";
     cout<<"TP : " << TP << "\n";
     cout<<"FP : " << FP << "\n";
     cout<<"TN : " << TN << "\n";
     cout<<"FN : " << FN << "\n";
+    cout<<"\nElapsed Time: "<<time<<"ms"<<endl;    
 }
 
 int main()
 {
-    ifstream input_file("susy.csv");
-    // using a the first few rows of the data set during development
-
-    vector<data_> point_list;
-    string consume;
-    getline(input_file, consume);
-    while(!input_file.eof())
+    int total_num_lines = 5000001;
+    cout<<"\n------------------------\n\tV1\n------------------------\n";
+    for (int dat_count = 0; dat_count < 3; dat_count++ )
     {
-        bool data_reading_exception_done = false;
-        string row;
-        data_ holder;
-        getline(input_file, row);
-        stringstream temp_stream(row);
-        int i = 0;
-        while (!temp_stream.eof())
+        PRED_POS = 0,TP = 0,FP = 0,TN = 0,FN = 0;
+        ifstream input_file("susy.csv");
+        // using a the first few rows of the data set during development
+
+        vector<data_> point_list;
+        string consume;
+        getline(input_file, consume);
+        int data_limit_counter = 0;
+        while(!input_file.eof())
         {
-            string word;
-            getline(temp_stream, word, ',');
-            if(word=="")
-            {   //This condition is to fix '\n' at the end of the csv
-                data_reading_exception_done = true;break;       
+            bool data_reading_exception_done = false;
+            string row;
+            data_ holder;
+            getline(input_file, row);
+            stringstream temp_stream(row);
+            int i = 0;
+            while (!temp_stream.eof())
+            {
+                string word;
+                getline(temp_stream, word, ',');
+                if(word=="")
+                {   //This condition is to fix '\n' at the end of the csv
+                    data_reading_exception_done = true;break;       
+                }
+
+                holder.setValue(i,word);
+                i++;
             }
-
-            holder.setValue(i,word);
-            i++;
+            if(data_reading_exception_done) break;
+            point_list.push_back(holder);        
+            data_limit_counter++;       
+            if(data_limit_counter >= (total_num_lines*dat_percentage[dat_count]/100))
+            {
+                cout<<"\n"<<data_limit_counter<<"<--\n";
+                break;
+            }
         }
-        if(data_reading_exception_done) break;
-        point_list.push_back(holder);        
-    }
-    input_file.close();
-//    print_dataset(point_list);
+        input_file.close();
 
-    for(int i=0;i<point_list.size();i++)
-    {    
-        calculate_linear_score_and_prob_and_set_data(point_list[i]);
-    }
-    getOutput(point_list.size());
+        struct timespec t_start, t_end;
+        clock_gettime(CLOCK_MONOTONIC, &t_start);    
 
+        for(int i=0;i<point_list.size();i++)
+        {    
+            calculate_linear_score_and_prob_and_set_data(point_list[i]);
+        }
+        
+        clock_gettime(CLOCK_MONOTONIC, &t_end);
+        double elapsed_ms =
+        (t_end.tv_sec - t_start.tv_sec) * 1000.0 +
+        (t_end.tv_nsec - t_start.tv_nsec) / 1e6;
+
+        getOutput(point_list.size(),elapsed_ms,dat_count);
+    }
     return 0;
 }
